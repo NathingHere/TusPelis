f171d01f3946f409710eac5711a51934cfe943de
bernabejr <58146817+bernabejr@users.noreply.github.com>
2020-05-18 19:15:56 +0200

    Diseño del Login y SplashScreen

---
+++ app/src/main/java/com/example/tuspelis/Login/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.tuspelis.Login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tuspelis.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+    }
+}
