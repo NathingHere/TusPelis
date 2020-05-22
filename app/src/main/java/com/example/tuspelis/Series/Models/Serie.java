952762f1eba16c9b8805decfd744cecb7396acb8
Pepeluu <pepelubjxd@gmail.com>
2020-05-22 18:50:38 +0200

    Commit FragmentPopular

--- app/src/main/java/com/example/tuspelis/Series/Models/Serie.java
+++ app/src/main/java/com/example/tuspelis/Series/Models/Serie.java
@@ -5,7 +5,6 @@ import android.os.Parcelable;
 
 import com.google.gson.annotations.SerializedName;
 
-import java.io.Serializable;
 import java.util.List;
 
 public class Serie implements Parcelable {
